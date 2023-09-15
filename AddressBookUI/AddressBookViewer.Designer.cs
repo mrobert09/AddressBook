555dc8fd762bc83961eb2c08c48ab17ba48801d9
mrobert09 <76024592+mrobert09@users.noreply.github.com>
2023-09-14 23:33:27 -0700

    Added main UI for address book. Currently unwired and non-functional.

---
+++ AddressBookUI/AddressBookViewer.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace AddressBookUI
+{
+    partial class AddressBookViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NewButton = new System.Windows.Forms.Button();
+            this.AddressListBox = new System.Windows.Forms.ListBox();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.SearchLabel = new System.Windows.Forms.Label();
+            this.SearchTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // NewButton
+            // 
+            this.NewButton.Location = new System.Drawing.Point(419, 30);
+            this.NewButton.Name = "NewButton";
+            this.NewButton.Size = new System.Drawing.Size(75, 32);
+            this.NewButton.TabIndex = 1;
+            this.NewButton.Text = "New";
+            this.NewButton.UseVisualStyleBackColor = true;
+            // 
+            // AddressListBox
+            // 
+            this.AddressListBox.FormattingEnabled = true;
+            this.AddressListBox.Location = new System.Drawing.Point(12, 30);
+            this.AddressListBox.Name = "AddressListBox";
+            this.AddressListBox.Size = new System.Drawing.Size(401, 264);
+            this.AddressListBox.TabIndex = 2;
+            // 
+            // EditButton
+            // 
+            this.EditButton.Location = new System.Drawing.Point(419, 68);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(75, 32);
+            this.EditButton.TabIndex = 3;
+            this.EditButton.Text = "Edit";
+            this.EditButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(419, 106);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 32);
+            this.DeleteButton.TabIndex = 4;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // SearchLabel
+            // 
+            this.SearchLabel.Location = new System.Drawing.Point(12, 9);
+            this.SearchLabel.Name = "SearchLabel";
+            this.SearchLabel.Size = new System.Drawing.Size(50, 18);
+            this.SearchLabel.TabIndex = 5;
+            this.SearchLabel.Text = "Search";
+            this.SearchLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // SearchTextBox
+            // 
+            this.SearchTextBox.Location = new System.Drawing.Point(68, 9);
+            this.SearchTextBox.Name = "SearchTextBox";
+            this.SearchTextBox.Size = new System.Drawing.Size(293, 20);
+            this.SearchTextBox.TabIndex = 8;
+            // 
+            // AddressBookViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 312);
+            this.Controls.Add(this.SearchTextBox);
+            this.Controls.Add(this.SearchLabel);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.AddressListBox);
+            this.Controls.Add(this.NewButton);
+            this.Name = "AddressBookViewer";
+            this.Text = "Address Book";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NewButton;
+        private System.Windows.Forms.ListBox AddressListBox;
+        private System.Windows.Forms.Button EditButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Label SearchLabel;
+        private System.Windows.Forms.TextBox SearchTextBox;
+    }
+}
